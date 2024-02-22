20fcfe852617760de43e3ddce4beba24cfcdd462
Thainara Onofre <thainaraonofre@outlook.com>
2024-02-22 16:15:04 -0300

    Validando o objeto UsuarioSenhaDto

--- src/main/java/com/thainaraonofre/demoparkapi/web/dto/UsuarioSenhaDTO.java
+++ src/main/java/com/thainaraonofre/demoparkapi/web/dto/UsuarioSenhaDTO.java
@@ -1,12 +1,23 @@
 package com.thainaraonofre.demoparkapi.web.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 
 @Getter @Setter @NoArgsConstructor @AllArgsConstructor @ToString
 public class UsuarioSenhaDTO {
 
+
+    @NotBlank
+    @Size(min = 6, max = 6)
     private String senhaAtual;
+
+    @NotBlank
+    @Size(min = 6, max = 6)
     private String novaSenha;
+
+    @NotBlank
+    @Size(min = 6, max = 6)
     private String confirmaSenha;
 
 }
