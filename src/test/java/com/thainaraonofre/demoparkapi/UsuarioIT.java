bf4a213289bada6aedeea411b47fed49145a47c9
Thainara Onofre <thainaraonofre@outlook.com>
2024-03-08 21:45:29 -0300

    Teste ponta a ponta para criar um novo usuário

---
+++ src/test/java/com/thainaraonofre/demoparkapi/UsuarioIT.java
@@ -0,0 +1,17 @@
+package com.thainaraonofre.demoparkapi;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+@SpringBootTest (webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Sql(scripts = "/sql/usuarios-insert.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+@Sql(scripts = "/sql/usuarios-delete.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
+public class UsuarioIT {
+
+    @Autowired
+    WebTestClient testClient;
+
+
+}
