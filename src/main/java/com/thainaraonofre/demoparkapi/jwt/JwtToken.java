792dfce5c99be99811dcf12cfa0fee97ece19129
Thainara Onofre <thainaraonofre@outlook.com>
2024-05-02 16:37:04 -0300

    JWT

---
+++ src/main/java/com/thainaraonofre/demoparkapi/jwt/JwtToken.java
@@ -0,0 +1,17 @@
+package com.thainaraonofre.demoparkapi.jwt;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter @Setter
+public class JwtToken {
+
+    private String token;
+
+
+}
