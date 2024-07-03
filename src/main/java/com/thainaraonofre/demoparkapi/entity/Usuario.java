89005d30a0c13ef88b0a6e88338544ef0403cd0b
Thainara Onofre <thainaraonofre@outlook.com>
2024-07-03 20:51:38 -0300

    Habilitando o sistema de auditoria

--- src/main/java/com/thainaraonofre/demoparkapi/entity/Usuario.java
+++ src/main/java/com/thainaraonofre/demoparkapi/entity/Usuario.java
@@ -4,16 +4,20 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.io.Serializable;
 import java.time.LocalDateTime;
 import java.util.Objects;
 
-@Getter
-@Setter
-@NoArgsConstructor
+@Getter @Setter @NoArgsConstructor
 @Entity
 @Table(name = "usuarios")
+@EntityListeners(AuditingEntityListener.class)
 public class Usuario implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -27,12 +31,16 @@ public class Usuario implements Serializable {
     @Column(name = "role", nullable = false, length = 25)
     private Role role = Role.ROLE_CLIENTE;
 
+    @CreatedDate
     @Column(name = "data_criacao")
     private LocalDateTime dataCriacao;
+    @LastModifiedDate
     @Column(name = "data_modificacao")
     private LocalDateTime dataModificacao;
+    @CreatedBy
     @Column(name = "criado_por")
     private String criadoPor;
+    @LastModifiedBy
     @Column(name = "modificado_por")
     private String modificadoPor;
 
