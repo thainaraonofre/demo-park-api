a183183830f139679bea82def918ed3a74b938ca
Thainara Onofre <thainaraonofre@outlook.com>
2024-07-05 20:18:47 -0300

    Permissão de acesso para administrador ao recurso de localização pelo Id

--- src/main/java/com/thainaraonofre/demoparkapi/web/controller/UsuarioController.java
+++ src/main/java/com/thainaraonofre/demoparkapi/web/controller/UsuarioController.java
@@ -16,6 +16,7 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -59,6 +60,7 @@ public class UsuarioController {
 
     )
     @GetMapping("/{id}")
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<UsuarioResponseDTO> getById(@PathVariable Long id) {
         Usuario user = usuarioService.buscarPorId(id);
         return ResponseEntity.ok(UsuarioMapper.toDto(user));
