3dad3b10cf53c1ba6284820399f1d114bbc1d85d
Thainara Onofre <thainaraonofre@outlook.com>
2024-08-22 15:40:32 -0300

    Preparando a operação de autenticação para os testes

---
+++ src/test/java/com/thainaraonofre/demoparkapi/JwtAuthentication.java
@@ -0,0 +1,31 @@
+package com.thainaraonofre.demoparkapi;
+
+
+import com.thainaraonofre.demoparkapi.jwt.JwtToken;
+import com.thainaraonofre.demoparkapi.web.dto.UsuarioLoginDTO;
+import org.springframework.http.HttpHeaders;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+import java.util.function.Consumer;
+
+public class JwtAuthentication {
+
+    public static Consumer<HttpHeaders> getHeaderAuthorization(WebTestClient client, String username, String password){
+        String token = client
+                .post()
+                .uri("/api/v1/auth")
+                .bodyValue(new UsuarioLoginDTO(username, password))
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody(JwtToken.class)
+                .returnResult().getResponseBody().getToken();
+        return headers -> headers.add(HttpHeaders.AUTHORIZATION, "Bearer " + token);
+
+    }
+
+
+
+
+}
+
+
